70110e95f2e678aaa9b5ac40ee627c54eca34311
Alexandre Courbot <gnurou@gmail.com>
2010-02-18 15:09:00 +0900

    Added missing kanji page generation

--- updatepublic.sh
+++ updatepublic.sh
@@ -7,5 +7,6 @@ tar czf currentdata.tar.gz currentdata
 gzip kanjivg.xml
 scp kanjivg.xml.gz gnurou@gnurou.org:/srv/http/kanjivg/upload/Main/kanjivg-latest.xml.gz
 scp currentdata.tar.gz gnurou@gnurou.org:/home/gnurou
+scp Main.StrokeCountMismatch gnurou@gnurou.org:/srv/http/kanjivg/wiki.d
 ssh gnurou@gnurou.org "cd /srv/http/kanjivg ; rm -Rf currentdata ; tar xfz /home/gnurou/currentdata.tar.gz ; rm /home/gnurou/currentdata.tar.gz"
 
