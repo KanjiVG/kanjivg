614cc04551f871370025fb72974f8fd3fe075956
Alexandre Courbot <gnurou@gmail.com>
2010-03-23 14:15:08 +0900

    Fixed typo

--- updatepublic.sh
+++ updatepublic.sh
@@ -9,5 +9,5 @@ gzip $outFile
 scp $outFile.gz gnurou@gnurou.org:/srv/http/kanjivg/upload/Main/
 scp currentdata.tar.gz gnurou@gnurou.org:/home/gnurou
 scp Main.StrokeCountMismatch Main.MissingKanji gnurou@gnurou.org:/srv/http/kanjivg/wiki.d
-ssh gnurou@gnurou.org "cd /srv/http/kanjivg ; rm -Rf currentdata ; tar xfz /home/gnurou/currentdata.tar.gz ; rm /home/gnurou/currentdata.tar.gz ; cd upload/Main ; ln -sf $outFile.gz kanjivg-latest.xml-gz"
+ssh gnurou@gnurou.org "cd /srv/http/kanjivg ; rm -Rf currentdata ; tar xfz /home/gnurou/currentdata.tar.gz ; rm /home/gnurou/currentdata.tar.gz ; cd upload/Main ; ln -sf $outFile.gz kanjivg-latest.xml.gz"
 
