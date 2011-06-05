555f0a40ee6992c3b785bcf408aa398e2539f60d
Alexandre Courbot <gnurou@gmail.com>
2011-06-05 20:36:39 +0900

    Update release upload script

--- updatepublic.sh
+++ updatepublic.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-rm -Rf kanjivg.xml.gz data
+rm -Rf kanjivg kanjivgMismatch
 ./mergexml.py
-outFile="kanjivg-`date +\"%Y%m%d\"`.tar.gz"
-tar cvzf $outFile data
-gzip $outFile
-scp $outFile.gz gnurou@gnurou.org:/srv/http/kanjivg/upload/Main/
-scp Main.StrokeCountMismatch Main.MissingKanji gnurou@gnurou.org:/srv/http/kanjivg/wiki.d
+outFile="kanjivg-ng-`date +\"%Y%m%d\"`.zip"
+zip -r $outFile kanjivg
+scp $outFile gnurou@gnurou.org:/srv/http/kanjivg/upload/Main/
+#scp Main.StrokeCountMismatch Main.MissingKanji gnurou@gnurou.org:/srv/http/kanjivg/wiki.d
 #ssh gnurou@gnurou.org "cd /srv/http/kanjivg ; rm -Rf currentdata ; tar xfz /home/gnurou/currentdata.tar.gz ; rm /home/gnurou/currentdata.tar.gz ; cd upload/Main ; ln -sf $outFile.gz kanjivg-latest.xml.gz"
 
