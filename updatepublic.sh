f056bce44d7844b04b71b7949b9206cb3e8d4dce
Alexandre Courbot <gnurou@gmail.com>
2011-07-23 23:00:33 +0900

    Remove obsolete files

--- updatepublic.sh
+++ updatepublic.sh
@@ -3,7 +3,7 @@ d=`date +%Y%m%d`
 outFileOne="kanjivg-$d.xml.gz"
 outFileAll="kanjivg-$d-all.zip"
 zip -r $outFileAll kanji
-python createsinglerelease.py
+python kvg.py release
 gzip -c kanjivg.xml >$outFileOne
 scp $outFileOne $outFileAll gnurou@gnurou.org:/srv/http/kanjivg/upload/Main/
 ssh gnurou@gnurou.org "ln -sf $outFileOne /srv/http/kanjivg/upload/Main/kanjivg-latest.xml.gz"
