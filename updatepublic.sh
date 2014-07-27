355b9ee1d1c9464d5536363b62093feea8931a60
ospalh <ospalh@gmail.com>
2014-07-27 17:05:32 +0200

    Also zip up only the main (i.e. not variant) kanji.

--- updatepublic.sh
+++ updatepublic.sh
@@ -2,7 +2,9 @@
 d=`date +%Y%m%d`
 outFileOne="kanjivg-$d.xml.gz"
 outFileAll="kanjivg-$d-all.zip"
-zip -r $outFileAll kanji
+outFileMain="kanjivg-$d-main.zip"
+zip -r $outFileAll kanji/*.svg
+zip -r $outFileMain kanji/?????.svg
 ./kvg.py release
 gzip -c kanjivg.xml >$outFileOne
 #scp $outFileOne $outFileAll gnurou@gnurou.org:/srv/http/kanjivg/upload/Main/
