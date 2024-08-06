d19f302325e10d3ef32100f0c434dcf070db3f17
Ben Bullock <benkasminbullock@gmail.com>
2024-08-06 09:35:53 +0900

    Fix broken stroke types in 08207-LeFst.svg

--- updatepublic.sh
+++ updatepublic.sh
@@ -3,7 +3,10 @@ d=`date +%Y%m%d`
 outFileOne="kanjivg-$d.xml.gz"
 outFileAll="kanjivg-$d-all.zip"
 outFileMain="kanjivg-$d-main.zip"
-zip -r $outFileAll kanji/*.svg
-zip -r $outFileMain kanji/?????.svg
+zip -rq $outFileAll kanji/*.svg
+zip -rq $outFileMain kanji/?????.svg
 ./kvg.py release
 gzip -c kanjivg.xml >$outFileOne
+release="r$d"
+gh release create --generate-notes --prerelease $release $outFileOne $outFileAll $outFileMain
+
