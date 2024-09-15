34b9008242a072957a4f88d85c9d2cb730f38320
Rick Gray <rickdgray@outlook.com>
2024-09-15 14:29:16 -0500

    initial ci/cd

---
+++ src/kanjivg/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata as metadata
+
+__version__ = metadata.version(__name__)
+__author__ = metadata.metadata(__name__)['Author']
+__license__ = metadata.metadata(__name__)['License']
+__all__ = [__name__]
+
+from .kanjivg import *
---
+++ src/kanjivg/kanjivg.py
@@ -0,0 +1,3 @@
+class kanjivg:
+    def __init__(self) -> None:
+        pass
